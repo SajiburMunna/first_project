b1812055e2e176202c96044c6c750022d1bda292
SajiburMunna <sajiburmunna087@gmail.com>
2021-01-23 01:25:32 +0600

    code

--- javascript.js
+++ javascript.js
@@ -1,6 +1,38 @@
-console.log(087);
-console.log("sajiburmunna");
-var m=10;
-var n=12;
-var c=m+n;
-console.log(c);
+// // // // console.log(087);
+// // // // console.log("sajiburmunna");
+// // // // var m=10;
+// // // // var n=12;
+// // // // var c=m+n;
+// // // // console.log(c);
+// // // // console.log(typeof n);
+// // //   console.log(undefined == null);
+
+// // // var age =18;
+// // // if (age =>18){
+// // //     console.log('Yes');
+// // // }
+// // // else{
+// // //     console.log('Nope');
+// // // }
+
+
+// // var name =' sajibur munna';
+// // for (var i=0;i<10;i++)
+// // {
+// //     console.log('Hello,'+ name);
+// // }
+
+ var names=[1,3,4,5,3];
+// names[0]='sajib';
+// names[1]='she' ;
+// names[2]='you';
+// //console.log(names[0]);
+// for(var i=0;i<names.length;i++){
+//     console.log(names[i].toUpperCase());
+// }
+
+console.log(names.length);
+console.log(names.length-1);
+console.log(names.indexOf(1));
+
+
